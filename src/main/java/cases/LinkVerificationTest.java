4863b1ab3017f18c019d627fefe922f7c0d1c296
gabriela.rojas <gabyrocas05@gmail.com>
2016-07-05 13:59:33 -0600

    first commit

---
+++ src/main/java/cases/LinkVerificationTest.java
@@ -0,0 +1,7 @@
+package cases;
+
+/**
+ * Created by gabriela.rojas on 6/29/16.
+ */
+public class LinkVerificationTest extends TestCase {
+}
---
+++ src/main/java/suites/Suite.java
@@ -0,0 +1,7 @@
+package suites;
+
+/**
+ * Created by gabriela.rojas on 6/29/16.
+ */
+abstract public class Suite {
+}
