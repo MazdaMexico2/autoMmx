de86196e166799f5993e71be1874757939568c51
alina.viquez <alina.viquez@possible.com>
2017-05-17 18:41:37 -0600

    Adding dropdownHeader class

--- src/main/java/headerDropdown.java
+++ src/main/java/headerDropdown.java
@@ -2,10 +2,6 @@
  * Created by alina.viquez on 5/1/17.
  */
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Properties;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -15,9 +11,13 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-//pablo
-public class testBrowser {
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
+
+//pablo
+public class headerDropdown {
 
     WebDriver driver;
     Properties configFile;
@@ -28,9 +28,7 @@ public class testBrowser {
         //driver = new ChromeDriver();
         driver = new FirefoxDriver();
         //driver = new SafariDriver();
-
-        //Test Alina #2
-
+        configFile = new Properties();
     }
 
     @Test
@@ -39,18 +37,34 @@ public class testBrowser {
         JavascriptExecutor jse = (JavascriptExecutor) driver;
 
 
-        /* Define URL to test*/
-        driver.get("https://www.mazda.mx/");
+        /* Define URL to test */
+        driver.get("https://www.mazda.mx");
 
         /*Maximize Window and load*/
         driver.manage().window().maximize();
-        jse.executeScript("window.scrollBy(0,550)");
+        //jse.executeScript("window.scrollBy(0,550)");
         Thread.sleep(2000);
+
+        List<WebElement> menu = driver.findElements(By.cssSelector(".mdp-navigation-global__menu ul li"));
+        for(int i =0;i<menu.size();i++) {
+            menu.get(i).click();
+            String elementClass= menu.get(i).getAttribute("class");
+
+            if (elementClass != null && elementClass.contains("active")){
+                menu.get(i).click();
+                System.out.print(" Dropdown of position"  + i + " displays ok ->");
+            }else{
+                System.out.print(" Drop down is not displaying or this position does not have dropdown");
+            }
+        }
+
     }
 
     @AfterMethod
     public void tearDown() {
+
         driver.quit();
     }
+
 
 }
