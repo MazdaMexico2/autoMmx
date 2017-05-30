8925dfe5e16675d8d6e6c9ef3cf778f67542a770
naserba <naserba.x@gmail.com>
2017-05-30 15:33:37 -0500

    update validator
    update validator section

--- src/main/java/Testcases/mx/footer/footerTestLinks.java
+++ src/main/java/Testcases/mx/footer/footerTestLinks.java
@@ -34,15 +34,19 @@ public class footerTestLinks {
 
     @BeforeMethod
     public void setUp() throws IOException {
-        if( propsmmx.getProperty("device") == "PC"){
+
+        if( propsmmx.getProperty("device").equalsIgnoreCase("PC")){
             System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\chromedriver.exe");
             System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "\\geckodriver.exe");
         } else {
             System.setProperty("webdriver.chrome.driver", "chromedriver");
         }
-        //driver = new ChromeDriver();
+
+        // driver = new ChromeDriver();
         driver = new FirefoxDriver();
         //driver = new SafariDriver();
+
+        //Test Alina #2
 
     }
 
